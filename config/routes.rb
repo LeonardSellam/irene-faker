df648f09ee0aacc6c9abc50d5c88562f35afd125
Leonard Sellam <leonard@honestica.com>
2018-01-25 16:53:48 +0100

    default rooting

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :document_references, only: [:index, :show]
+
+  root to: 'admin/document_references#index'
 end
